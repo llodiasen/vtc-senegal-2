9bcb54e638f999e590158027c023cb1c71c596d9
wopa <wopallodia92@gmail.com>
2026-02-11 00:02:47 +0000

    Fix: ajout headers cache pour rÃ©soudre erreurs 404 chunks

--- next.config.js
+++ next.config.js
@@ -24,6 +24,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
     ];
   },
 };
